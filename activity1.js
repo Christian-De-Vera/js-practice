689b39ee7a5709938173f1bf757a06af0f0738e8
Christian <zen.chishiki404@gmail.com>
2024-09-22 15:24:47 +0800

    new

---
+++ activity1.js
@@ -0,0 +1,16 @@
+// Vvariabe to store my name
+let name = "Christian";
+
+// Variable to store ur life goal
+let whatDoYouWannaBecomeInYourLife = "programmer";
+
+// Variable to store my gender
+let gender = "Male";
+
+// Variable to store my Facebook account name
+let facebookAcct = "Christian De Vera";
+
+console.log("Name:", name);
+console.log("Life Goal:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Facebook Account:", facebookAcct);
